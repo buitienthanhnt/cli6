eb4f974f01e5d67ed0cc136a19af8e62fa52d720
thanhnt <buitienthanhnt@gmail.com>
2023-12-03 00:24:05 +0700

    main: Update for babel-plugin-module-resolver

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,28 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-};
+			presets: ['module:metro-react-native-babel-preset'],
+			plugins: [
+				[
+					require.resolve('babel-plugin-module-resolver'),
+					{
+						root: ['./src'],
+						extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+						alias: {
+						'ios': './ios/',
+						'android': './android/',
+						'@assets': './src/assets',
+						'@config': './src/config',
+						'@hooks': './src/hooks',
+						'@hooks': './src/hooks',
+						'@screens': './src/screens',
+						'@utils': './src/utils',
+						'@bottoms': './src/bottoms',
+						// '@redux': './src/redux',
+						// '@services': './src/services',
+						// '@styles': './src/styles',
+						// '@types': './src/types',
+						},
+					},
+				],
+
+			]
+		};

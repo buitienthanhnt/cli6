f57d38230a8d3be83e09abe8829b3ecc270fbcc7
thanhnt <buitienthanhnt@gmail.com>
2023-12-17 01:05:28 +0700

    up

--- src/screens/CodeScreen/components/animated/Reanimated.js
+++ src/screens/CodeScreen/components/animated/Reanimated.js
@@ -5,12 +5,13 @@ import Animated, { useSharedValue, withSpring, withTiming, useAnimatedStyle, use
 // import { styled, useColorScheme } from "nativewind";
 
 const Reanimated1 = () => {
+    const [text, setText] = useState('');
     const width = useSharedValue(100); // dung gia tri co ban
 
     const translateX = useSharedValue(0);
     const translateY = useSharedValue(0);
-    const _w = useSharedValue(120);
-    const _h = useSharedValue(120);
+    const _w = useSharedValue(124);
+    const _h = useSharedValue(124);
     const r = useSharedValue(0);
     const animatedStyles = useAnimatedStyle(() => ({
         transform: [
@@ -22,12 +23,14 @@ const Reanimated1 = () => {
     }));
 
     const position = useSharedValue('absolute');
-    const pTop = useSharedValue(20), pFeft = useSharedValue(20);
+    const pTop = useSharedValue(24), pFeft = useSharedValue(24);
+    const fSize = useSharedValue(12);
     const placeStyle = useAnimatedStyle(() => {
         return {
             top: withSpring(pTop.value),
             left: withSpring(pFeft.value),
-            position: position.value
+            position: position.value,
+            fontSize: withSpring(fSize.value)
         }
     })
 
@@ -40,34 +43,41 @@ const Reanimated1 = () => {
     }));
 
     return (
-        <View style={{ padding: 20 }}>
-            <Animated.View
-                style={{
-                    width: width,
-                    height: 100,
-                    backgroundColor: 'violet',
-                }}
-            >
-                <Text>213123</Text>
-            </Animated.View>
+        <TouchableWithoutFeedback style={{ padding: 24 }} onPress={() => {
+            if (!text) {
+                pFeft.value = pTop.value = 24;
+                position.value = 'absolute';
+                fSize.value = 12
+            }
+            Keyboard.dismiss()
+        }} accessible={false}>
+            <View>
+                <Animated.View
+                    style={{
+                        width: width,
+                        height: 100,
+                        backgroundColor: 'violet',
+                    }}
+                >
+                </Animated.View>
 
-            <Animated.View style={[styles.box, animatedStyles]} />
-
-            <TouchableWithoutFeedback onPress={() => {
-                Keyboard.dismiss()
-            }} accessible={false}>
-
+                <Animated.View style={[styles.box, animatedStyles]} />
+                <CustomInput></CustomInput>
+                <CustomInput></CustomInput>
                 <View style={{
                     width: 'auto',
-                    borderRadius: 10, padding: 10,
-                    borderWidth: 1, borderColor: 'red',
+                    borderRadius: 10,
+                    padding: 10,
+                    // borderWidth: 1, 
+                    // borderColor: 'red',
                 }}>
                     <Animated.Text style={placeStyle}>place holder</Animated.Text>
                     <TextInput style={{
                         borderWidth: 1, borderRadius: 10,
                         borderColor: 'green',
                         padding: 5,
-                        marginTop: 5
+                        marginTop: 5,
+                        fontSize: 15
                     }}
                         // placeholder="placeholder text"
                         // onPressOut={()=>{
@@ -77,26 +87,32 @@ const Reanimated1 = () => {
                         //     console.log('onPressIn');
                         // }}
                         onFocus={() => {
-                            pFeft.value -= 20;
-                            pTop.value -= 20;
+                            if (pFeft.value > 0 && pTop.value > 0) {
+                                pFeft.value -= 20;
+                                pTop.value -= 24;
+                                fSize.value = 16;
+                            }
+
                             position.value = withSpring('relative')
                         }}
+                        onChangeText={text => setText(text)}
+                        value={text}
                     ></TextInput>
                 </View>
-            </TouchableWithoutFeedback>
 
-            <Text></Text>
+                <Button title="onpress"
+                    style={{ marginTop: 10 }}
+                    onPress={() => {
+                        width.value = withSpring(width.value + 24)
+                        translateX.value += 24;
+                        translateY.value -= 10;
+                        _w.value -= 10;
+                        _h.value -= 10
+                    }}></Button>
 
-            <Button title="onpress"
-                style={{ marginTop: 10 }}
-                onPress={() => {
-                    width.value = withSpring(width.value + 20)
-                    translateX.value += 20;
-                    translateY.value -= 10;
-                    _w.value -= 10;
-                    _h.value -= 10
-                }}></Button>
-        </View>
+            </View>
+        </TouchableWithoutFeedback>
+
     )
     // demo nativeWind
     // const StyledView = styled(View); 
@@ -109,6 +125,63 @@ const Reanimated1 = () => {
     // )
 }
 
+const CustomInput = () => {
+    const [text, setText] = useState('');
+    const position = useSharedValue('absolute');
+    const pTop = useSharedValue(24), pFeft = useSharedValue(24);
+    const fSize = useSharedValue(12);
+    const placeStyle = useAnimatedStyle(() => {
+        return {
+            top: withSpring(pTop.value),
+            left: withSpring(pFeft.value),
+            position: position.value,
+            fontSize: withSpring(fSize.value)
+        }
+    })
+
+    return (
+
+        <TouchableWithoutFeedback style={{ padding: 24 }} onPress={() => {
+            if (!text) {
+                pFeft.value = pTop.value = 24;
+                position.value = 'absolute';
+                fSize.value = 12
+            }
+            Keyboard.dismiss()
+        }} accessible={false}>
+            <View style={{
+                width: 'auto',
+                borderRadius: 10,
+                padding: 10,
+                // borderWidth: 1, 
+                // borderColor: 'red',
+            }}>
+                <Animated.Text style={placeStyle}>place holder</Animated.Text>
+                <TextInput style={{
+                    borderWidth: 1, borderRadius: 10,
+                    borderColor: 'green',
+                    padding: 5,
+                    marginTop: 5,
+                    fontSize: 15
+                }}
+                    onFocus={() => {
+                        if (pFeft.value > 0 && pTop.value > 0) {
+                            pFeft.value -= 20;
+                            pTop.value -= 24;
+                            fSize.value = 16;
+                        }
+
+                        position.value = withSpring('relative')
+                    }}
+                    onChangeText={text => setText(text)}
+                    value={text}
+                ></TextInput>
+            </View>
+        </TouchableWithoutFeedback>
+
+    )
+}
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -117,8 +190,8 @@ const styles = StyleSheet.create({
     },
     box: {
         backgroundColor: '#b58df1',
-        borderRadius: 20,
-        marginVertical: 50,
+        borderRadius: 24,
+        // marginVertical: 50,
     },
 });
 

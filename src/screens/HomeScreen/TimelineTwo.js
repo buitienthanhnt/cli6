974fd233c45fc5e73a565b3d20da008f812bd11f
buitienthanhnt@gmail.com <namtanvietnam>
2024-02-23 14:55:54 +0700

    dev: Update code for design Home screen with timeLine.

--- src/screens/HomeScreen/TimelineTwo.js
+++ src/screens/HomeScreen/TimelineTwo.js
@@ -1,6 +1,7 @@
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
+ * https://github.com/Eugnis/react-native-timeline-flatlist/blob/master/examples/Example/pages/dotExample.js
  * @flow
  */
 
@@ -25,41 +26,41 @@ export default class TimelineTwo extends Component {
                 title: 'Archery Training',
                 description: 'The Beginner Archery and Beginner Crossbow course does not require you to bring any equipment,',
                 lineColor: '#009688',
-                // icon: require('../img/archery.png'),
+                icon: require('./img/archery.png'),
                 imageUrl: 'https://cloud.githubusercontent.com/assets/21040043/24240340/c0f96b3a-0fe3-11e7-8964-fe66e4d9be7a.jpg'
             },
             {
                 time: '10:45',
                 title: 'Play Badminton',
                 description: 'Badminton is a racquet sport played using racquets to hit a shuttlecock across a net.',
-                // icon: require('../img/badminton.png'),
+                icon: require('./img/badminton.png'),
                 imageUrl: 'https://cloud.githubusercontent.com/assets/21040043/24240405/0ba41234-0fe4-11e7-919b-c3f88ced349c.jpg'
             },
             {
                 time: '12:00',
                 title: 'Lunch',
-                // icon: require('../img/lunch.png'),
+                icon: require('./img/lunch.png'),
             },
             {
                 time: '14:00',
                 title: 'Watch Soccer',
                 description: 'Team sport played between two teams of eleven players with a spherical ball. ',
                 lineColor: '#009688',
-                // icon: require('../img/soccer.png'),
+                icon: require('./img/soccer.png'),
                 imageUrl: 'https://cloud.githubusercontent.com/assets/21040043/24240419/1f553dee-0fe4-11e7-8638-6025682232b1.jpg'
             },
             {
                 time: '16:30',
                 title: 'Go to Fitness center',
                 description: 'Look out for the Best Gym & Fitness Centers around me :)',
-                // icon: require('../img/dumbbell.png'),
+                icon: require('./img/dumbbell.png'),
                 imageUrl: 'https://cloud.githubusercontent.com/assets/21040043/24240422/20d84f6c-0fe4-11e7-8f1d-9dbc594d0cfa.jpg'
             },
             {
                 time: '18:30',
                 title: 'Go to homer',
                 description: 'Look out for the Best Gym & Fitness Centers around me :)',
-                // icon: require('../img/dumbbell.png'),
+                icon: require('./img/dumbbell.png'),
                 imageUrl: 'https://cloud.githubusercontent.com/assets/21040043/24240422/20d84f6c-0fe4-11e7-8f1d-9dbc594d0cfa.jpg'
             }
         ]
@@ -83,7 +84,8 @@ export default class TimelineTwo extends Component {
                     style={styles.list}
                     data={this.data}
                     circleSize={14}
-                    circleColor='rgb(45,156,219)'
+                    circleColor='rgb(45,156,219)'  // màu của hình tròn
+                    // circleColor='rgba(0,0,0,0)'
                     lineColor='rgb(45,156,219)'
                     timeContainerStyle={{ minWidth: 52, }}
                     timeStyle={{ textAlign: 'center', backgroundColor: '#ff9797', color: 'white', padding: 4, borderRadius: 8 }}
@@ -91,7 +93,7 @@ export default class TimelineTwo extends Component {
                     options={{
                         style: { paddingTop: 0 }
                     }}
-                    //   innerCircle={'icon'}
+                    // innerCircle={'icon'} // dùng biểu tượng ảnh
                     onEventPress={this.onEventPress}
                     separator={false}
                     titleStyle={{ marginTop: 0 }}

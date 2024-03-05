4a804a87b75d482027fc40ad06e32f6353854bcc
buitienthanhnt@gmail.com <namtanvietnam>
2024-03-05 15:43:14 +0700

    Dev-carousel: Update for next button

--- src/screens/PaperScreen/element/PaperCarousel.js
+++ src/screens/PaperScreen/element/PaperCarousel.js
@@ -1,15 +1,17 @@
-import { useCallback } from "react";
+import { useCallback, useRef, useState } from "react";
 
-import { View, Text, Dimensions, FlatList, ImageBackground } from 'react-native';
+import { View, Text, Dimensions, FlatList, ImageBackground, Button, TouchableOpacity } from 'react-native';
 
 const PaperCarousel = ({ slider_images }) => {
     const height = Math.floor(Dimensions.get('screen').width) / 4 * 2;
+    const active = useRef(0);
+    const ref = useRef(FlatList);
 
     const renderItem = useCallback(({ item, index }) => {
         return (
             <ImageBackground
                 source={{ uri: item.value }}
-                imageStyle={{ borderRadius: 10, resizeMode: 'cover'}}
+                imageStyle={{ borderRadius: 10, resizeMode: 'cover' }}
                 style={{
                     width: Dimensions.get('screen').width - 20,
                     height: height,
@@ -19,12 +21,12 @@ const PaperCarousel = ({ slider_images }) => {
                     borderRadius: 10
                 }}
             >
-                <Text style={{ 
+                <Text style={{
                     width: '100%',
-                    color: 'white', 
-                    fontSize: 16, 
-                    textAlign: 'center', 
-                    fontWeight: 600 
+                    color: 'white',
+                    fontSize: 16,
+                    textAlign: 'center',
+                    fontWeight: 600
                 }}>
                     {item.description}
                 </Text>
@@ -35,21 +37,47 @@ const PaperCarousel = ({ slider_images }) => {
         return null;
     }
     return (
-        <View 
-            style={{ 
-                height: height + 10,
-                paddingVertical: 5
-            }}>
+        <View>
             <FlatList
+                contentContainerStyle={{
+                    height: height + 10,
+                    paddingVertical: 5,
+                    gap: 5
+                }}
+                ref={ref}
                 horizontal={true}
                 data={slider_images}
                 extraData={slider_images}
                 keyExtractor={(item, index) => index}
                 renderItem={renderItem}
                 pagingEnabled={true}
-                contentContainerStyle={{ gap: 5 }}
-            ></FlatList>
+                showsHorizontalScrollIndicator={false}
+            >
+
+            </FlatList>
+            <TouchableOpacity style={{ position: 'absolute', top: height / 2, backgroundColor: 'rgba(55, 123, 247, 0.2)', padding: 2, borderRadius: 4,}}
+                onPress={() => {
+                    if (active.current == 0) {
+                        return;
+                    }
+                    ref.current.scrollToIndex({ index: active.current - 1 });
+                    active.current = active.current - 1;
+                }}>
+                <Text style={{ color: 'white', fontSize: 16, fontWeight: 600 }}>Prev</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={{ position: 'absolute', top: height / 2, right: 5, backgroundColor: 'rgba(55, 123, 247, 0.2)', padding: 2, borderRadius: 4 }}
+                onPress={() => {
+                    if (active.current >= slider_images.length - 1) {
+                        return;
+                    }
+                    ref.current.scrollToIndex({ index: active.current + 1 });
+                    active.current = active.current + 1;
+                }}
+            >
+                <Text style={{ color: 'white', fontSize: 16, fontWeight: 600,}}>Next</Text>
+            </TouchableOpacity>
         </View>
+
     )
 }
 

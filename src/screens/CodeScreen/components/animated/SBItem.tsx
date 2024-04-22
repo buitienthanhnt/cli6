53cd6ccb618ebf78d6f6ab4717e2ab79c401b20d
tha nan <buitienthanhnt@gmail.com>
2024-04-22 21:13:37 +0700

    deev-typeScr: Update

--- src/screens/CodeScreen/components/animated/SBItem.tsx
+++ src/screens/CodeScreen/components/animated/SBItem.tsx
@@ -1,42 +1,56 @@
-import React from "react";
-import type { StyleProp, ViewStyle, ViewProps, ImageSourcePropType } from "react-native";
-import { LongPressGestureHandler } from "react-native-gesture-handler";
-import type { AnimateProps } from "react-native-reanimated";
-import Animated from "react-native-reanimated";
+import React from 'react';
+import type {StyleProp, ViewStyle, ViewProps} from 'react-native';
+import {LongPressGestureHandler} from 'react-native-gesture-handler';
+import type {AnimateProps} from 'react-native-reanimated';
+import Animated from 'react-native-reanimated';
 
-import Constants from "expo-constants";
+// import Constants from "expo-constants";
 
-import { SBImageItem } from "./SBImageItem";
-import { SBTextItem } from "./SBTextItem";
+import {SBImageItem} from './SBImageItem';
+import {SBTextItem} from './SBTextItem';
 
 interface Props extends AnimateProps<ViewProps> {
-  style?: StyleProp<ViewStyle>
-  index?: number
-  pretty?: boolean
-  showIndex?: boolean
-  img?: string,
-  title?: string,
-  onPress?: () => void,
+  style?: StyleProp<ViewStyle>;
+  index?: number;
+  pretty?: boolean;
+  showIndex?: boolean;
+  img?: string;
+  title?: string;
+  onPress?: () => void;
 }
 
-export const SBItem: React.FC<Props> = (props) => {
-  const { style, showIndex = true, index, pretty, img, title,testID, onPress, ...animatedViewProps } = props;
-  const enablePretty = Constants?.expoConfig?.extra?.enablePretty || false;
-  const [isPretty, setIsPretty] = React.useState(pretty || enablePretty);
+export const SBItem: React.FC<Props> = props => {
+  const {
+    style,
+    showIndex = true,
+    index,
+    pretty,
+    img,
+    title,
+    testID,
+    onPress,
+    ...animatedViewProps
+  } = props;
+
+  const [isPretty, setIsPretty] = React.useState(pretty);
   return (
     <LongPressGestureHandler
       onActivated={() => {
         setIsPretty(!isPretty);
-      }}
-    >
-      <Animated.View testID={testID} style={{ flex: 1 }} {...animatedViewProps}>
-        {isPretty || img
-          ? (
-            <SBImageItem style={style} index={index} onPress={onPress} showIndex={typeof index === "number" && showIndex} img={img} title={title} />
-          )
-          : (
-            <SBTextItem style={style} index={index} />
-          )}
+      }}>
+      <Animated.View testID={testID} style={{flex: 1}} {...animatedViewProps}>
+        {isPretty || img ? (
+          <SBImageItem
+            style={style}
+            index={index}
+            onPress={onPress}
+            showIndex={typeof index === 'number' && showIndex}
+            img={img}
+            title={title}
+          />
+        ) : (
+          <SBTextItem style={style} index={index} />
+        )}
       </Animated.View>
     </LongPressGestureHandler>
   );
--- src/screens/PaperScreen/PaperDetail.js
+++ src/screens/PaperScreen/PaperDetail.js
@@ -1,162 +1,202 @@
-import react, { useCallback, useEffect, useState, useRef } from "react";
-import { Dimensions, Image, ScrollView, RefreshControl, Text, View, StyleSheet, Button } from "react-native";
-import Config from "@config/Config";
-import IframeRenderer, { iframeModel } from '@native-html/iframe-plugin';   // npm install @native-html/iframe-plugin
-import RenderHTML from 'react-native-render-html';                          // npm install react-native-render-html
-import WebView from 'react-native-webview';                                 // npm install react-native-webview
-import perf from "@react-native-firebase/perf";
-import Comments from "./element/Comments";
-import DetailLike from "./element/DetailLike";
-import { PaperDetailContext } from "./PaperContext";
-import CarolParax from "@screens/CodeScreen/components/animated/CarolParax";
-import { layoutDimension } from "@styles/css";
-import { caroll } from "./api/datatest";
-import Carolsel from "@screens/AccountScreen/components/Carolsel";
-import PaperTag from "./element/PaperTag";
-import PaperCarousel from "./element/PaperCarousel";
+import react, {useCallback, useEffect, useState, useRef} from 'react';
+import {
+  Dimensions,
+  Image,
+  ScrollView,
+  RefreshControl,
+  Text,
+  View,
+  StyleSheet,
+  Button,
+} from 'react-native';
+import Config from '@config/Config';
+import IframeRenderer, {iframeModel} from '@native-html/iframe-plugin'; // npm install @native-html/iframe-plugin
+import RenderHTML from 'react-native-render-html'; // npm install react-native-render-html
+import WebView from 'react-native-webview'; // npm install react-native-webview
+import perf from '@react-native-firebase/perf';
+import Comments from './element/Comments';
+import DetailLike from './element/DetailLike';
+import {PaperDetailContext} from './PaperContext';
+import CarolParax from '@screens/CodeScreen/components/animated/CarolParax';
+import {layoutDimension} from '@styles/css';
+import {caroll} from './api/datatest';
+import Carolsel from '@screens/AccountScreen/components/Carolsel';
+import PaperTag from './element/PaperTag';
+import PaperCarousel from './element/PaperCarousel';
 
 const renderers = {
-    iframe: IframeRenderer
+  iframe: IframeRenderer,
 };
 
 const customHTMLElementModels = {
-    iframe: iframeModel
+  iframe: iframeModel,
 };
 
-const PaperDetail = ({ navigation, route }) => {
-    // use custom hook
-    // gan bien detail banfg gia tri bien data(detail = data)
-    // const {isLoading, data: detail, error} = useFect(Config.url + Config.api_request.getPaperDetail + (route.params?.paper_id || route.params.data.id));
-    const [detail, setDetail] = useState(null);
-    const [showWebview, setShowwebview] = useState(false);
-    const [refreshing, setRefreshing] = useState(false);
-    const [commentParent, setCommentParent] = useState(null);
-    const refRBSheet = useRef();
+const PaperDetail = ({navigation, route}) => {
+  // use custom hook
+  // gan bien detail banfg gia tri bien data(detail = data)
+  // const {isLoading, data: detail, error} = useFect(Config.url + Config.api_request.getPaperDetail + (route.params?.paper_id || route.params.data.id));
+  const [detail, setDetail] = useState(null);
+  const [showWebview, setShowwebview] = useState(false);
+  const [refreshing, setRefreshing] = useState(false);
+  const [commentParent, setCommentParent] = useState(null);
+  const refRBSheet = useRef();
 
-    useEffect(() => {
-        // console.log(route.params.data.id);
-        if (route.params.id != undefined) {
-            getDetailPaper(route.params.id);
-        } else {
-            getDetailPaper(route.params.data.id);
+  const getDetailPaper = useCallback(
+    async (id = 0) => {
+      const traceInitScreen = await perf().startTrace('detail_trace');
+      traceInitScreen.putMetric('hits', 1);
+      if (id) {
+        try {
+          setRefreshing(true);
+          const detail = await fetch(
+            Config.url + Config.api_request.getPaperDetail + id,
+          );
+          var result = await detail.json();
+          setRefreshing(false);
+          if (result) {
+            setDetail(result);
+          } else {
+            navigation.goBack();
+          }
+        } catch (error) {
+          navigation.goBack();
         }
-    }, [route.params]
-    );
+      }
+      // console.log(traceInitScreen);
+      await traceInitScreen.stop();
+    },
+    [navigation],
+  );
 
-    const getDetailPaper = useCallback(async (id = 0) => {
-        const traceInitScreen = await perf().startTrace('detail_trace');
-        traceInitScreen.putMetric("hits", 1);
-        if (id) {
-            try {
-                setRefreshing(true);
-                const detail = await fetch(Config.url + Config.api_request.getPaperDetail + id);
-                var result = await detail.json();
-                setRefreshing(false);
-                if (result) {
-                    setDetail(result);
-                } else {
-                    navigation.goBack();
-                }
-            } catch (error) {
-                navigation.goBack();
-            }
-        }
-        // console.log(traceInitScreen);
-        await traceInitScreen.stop()
-    }, []);
-
-    const onRefresh = () => {
-        if (route?.params?.data?.id) {
-            getDetailPaper(route.params.data.id);
-        }
+  useEffect(() => {
+    // console.log(route.params.data.id);
+    if (route.params.id != undefined) {
+      getDetailPaper(route.params.id);
+    } else {
+      getDetailPaper(route.params.data.id);
     }
+  }, [getDetailPaper, route.params]);
 
-    if (detail) {
-        if (showWebview) {
-            return <WebView source={{ uri: "www.topsy-fashion.nl" }} />
-        }
-        return (
-            <PaperDetailContext.Provider value={{paperId: detail.id, url: detail.url, title: detail.title, refRBSheet, commentParent, setCommentParent}}>
-                <ScrollView
-                    contentContainerStyle={{ paddingBottom: 20 }}
-                    showsVerticalScrollIndicator={false}
-                    showsHorizontalScrollIndicator={false}
-                    style={css.container}
-                    refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
-                >
-                    <Text style={{ fontSize: 18, fontWeight: "600", color: "green", textDecorationLine: "underline" }}>{detail.title}</Text>
-                    {/* <RenderHTML contentWidth={Dimensions.get("screen").width} source={{ html }}></RenderHTML> */}
-                    <PaperCarousel slider_images={detail.slider_images}></PaperCarousel>
-                    <RenderHTML
-                        renderers={renderers}
-                        WebView={WebView}
-                        source={{ html: detail?.conten || '' }}
-                        contentWidth={Dimensions.get("screen").width}
-                        customHTMLElementModels={customHTMLElementModels}
-                        defaultWebViewProps={
-                            {
-                                /* Any prop you want to pass to all WebViews */
-                            }
-                        }
-                        renderersProps={{
-                            iframe: {
-                                scalesPageToFit: true,
-                                webViewProps: {
-                                    /* Any prop you want to pass to iframe WebViews */
-                                }
-                            }
-                        }}
-                        onPress={(event) => { return undefined; }}
-                    />
-                    <DetailLike info={detail.info}></DetailLike>
-                    <PaperTag tags={detail?.tags}></PaperTag>
-                    <Comments paperId={detail.id}></Comments>
-                    <View style={{ height: 1, backgroundColor: "black" }}></View>
-                    <LastNews paper_id={route?.params?.data?.id || 1} navigation={navigation}></LastNews>
-                    <CarolParax data={caroll}></CarolParax>
-                    {/* <Button title="view in webview" onPress={() => {
+  const onRefresh = () => {
+    if (route?.params?.data?.id) {
+      getDetailPaper(route.params.data.id);
+    }
+  };
+
+  if (detail) {
+    if (showWebview) {
+      return <WebView source={{uri: 'www.topsy-fashion.nl'}} />;
+    }
+    return (
+      <PaperDetailContext.Provider
+        value={{
+          paperId: detail.id,
+          url: detail.url,
+          title: detail.title,
+          refRBSheet,
+          commentParent,
+          setCommentParent,
+        }}>
+        <ScrollView
+          contentContainerStyle={{paddingBottom: 20}}
+          showsVerticalScrollIndicator={false}
+          showsHorizontalScrollIndicator={false}
+          style={css.container}
+          refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+          }>
+          <Text
+            style={{
+              fontSize: 18,
+              fontWeight: '600',
+              color: 'green',
+              textDecorationLine: 'underline',
+            }}>
+            {detail.title}
+          </Text>
+          {/* <RenderHTML contentWidth={Dimensions.get("screen").width} source={{ html }}></RenderHTML> */}
+          <PaperCarousel slider_images={detail.slider_images} />
+          <RenderHTML
+            renderers={renderers}
+            WebView={WebView}
+            source={{html: detail?.conten || ''}}
+            contentWidth={Dimensions.get('screen').width}
+            customHTMLElementModels={customHTMLElementModels}
+            defaultWebViewProps={
+              {
+                /* Any prop you want to pass to all WebViews */
+              }
+            }
+            renderersProps={{
+              iframe: {
+                scalesPageToFit: true,
+                webViewProps: {
+                  /* Any prop you want to pass to iframe WebViews */
+                },
+              },
+            }}
+            onPress={event => {
+              return undefined;
+            }}
+          />
+          <DetailLike info={detail.info} />
+          <PaperTag tags={detail?.tags} />
+          <Comments paperId={detail.id} />
+          <View style={{height: 1, backgroundColor: 'black'}} />
+          <LastNews
+            paper_id={route?.params?.data?.id || 1}
+            navigation={navigation}
+          />
+          <CarolParax data={caroll} />
+          {/* <Button title="view in webview" onPress={() => {
                         setShowwebview(true)
                     }}></Button> */}
-                </ScrollView>
-            </PaperDetailContext.Provider>
-        );
-    } else {
-        return (
-            <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-                {/* <ActivityIndicator size="small" color="#0000ff" /> */}
-                <Image source={require("@assets/Ripple-1s-200px.gif")} style={{ width: 60, height: 60 }}></Image>
-            </View>);
-    }
-}
-
-const LastNews = (props) => {
-    const { paper_id, navigation } = props;
-    const [data, setData] = useState(['1', "2", "3", "4", "5"]);
-
-    const getRelatedPaper = async () => {
-        try {
-            let request_api = Config.url + Config.api_request.getRelatedPaper + paper_id;
-            const response = await fetch(request_api);
-            const _data = await response.json();
-            setData(_data?.["items"]);
-        } catch (error) { console.log(error); }
-    }
-    useEffect(() => {
-        // console.log(paper_id);
-    }, [])
-
-    return (
-        <Carolsel navigation={navigation}></Carolsel>
+        </ScrollView>
+      </PaperDetailContext.Provider>
     );
-}
+  } else {
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        {/* <ActivityIndicator size="small" color="#0000ff" /> */}
+        <Image
+          source={require('@assets/Ripple-1s-200px.gif')}
+          style={{width: 60, height: 60}}
+        />
+      </View>
+    );
+  }
+};
+
+const LastNews = props => {
+  const {paper_id, navigation} = props;
+  const [data, setData] = useState(['1', '2', '3', '4', '5']);
+
+  const getRelatedPaper = async () => {
+    try {
+      let request_api =
+        Config.url + Config.api_request.getRelatedPaper + paper_id;
+      const response = await fetch(request_api);
+      const _data = await response.json();
+      setData(_data?.items);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    // console.log(paper_id);
+  }, []);
+
+  return <Carolsel navigation={navigation} />;
+};
 
 const css = StyleSheet.create({
-    container: {
-        flex: 1,
-        paddingHorizontal: 8,
-        paddingBottom: layoutDimension.bottomTabHeight,
-        // backgroundColor: "#d6ffc6"
-    }
-})
+  container: {
+    flex: 1,
+    paddingHorizontal: 8,
+    paddingBottom: layoutDimension.bottomTabHeight,
+    // backgroundColor: "#d6ffc6"
+  },
+});
 
 export default PaperDetail;

59a390f584fc1c35a05983f05ecac7abfaab23ca
thanhnt <buitienthanhnt@gmail.com>
2024-04-25 16:16:16 +0700

    dev: update

--- src/screens/CodeScreen/components/animated/PanResponders.js
+++ src/screens/CodeScreen/components/animated/PanResponders.js
@@ -18,6 +18,10 @@ const PanResponders = () => {
   const radius = useSharedValue(0);
   const rotate = useSharedValue(0);
 
+  const [onX, setOnX] = useState(0);
+  const dX = useSharedValue(0);
+  const [show, setShow] = useState(false);
+
   const panGesture = Gesture.Pan()
     .onTouchesDown((e) => {
       // doi mau
@@ -27,8 +31,8 @@ const PanResponders = () => {
       radius.value = 8
 
       // thu nho
-      w.value = withTiming(55, {duration: 200})
-      h.value = withTiming(55, {duration: 200})
+      w.value = withTiming(55, { duration: 200 })
+      h.value = withTiming(55, { duration: 200 })
 
       // xoay
       // rotate.value = withRepeat(
@@ -49,9 +53,25 @@ const PanResponders = () => {
       runOnJS(setColor)('green') // doi lai mau
       radius.value = 0; // het bo vien
       // rotate.value = 0
-      w.value = withTiming(60, {duration: 200})
-      h.value = withTiming(60, {duration: 200})
+      w.value = withTiming(60, { duration: 200 })
+      h.value = withTiming(60, { duration: 200 })
     });
+
+  const showCom = Gesture.Pan().onTouchesDown((e) => {
+    // console.log('====================================');
+    // console.log(e);
+    // console.log('====================================');
+    runOnJS(setOnX)(e.changedTouches[0].x)
+    runOnJS(setShow)(true)
+    dX.value = e.changedTouches[0].x;
+  }).onUpdate((e) => {
+    dX.value = onX + e.translationX;
+  }
+  ).onEnd((e) => {
+    // 
+    runOnJS(setShow)(false);
+    runOnJS(setOnX)(onX+ e.translationX)
+  })
 
   const animatedStyle = useAnimatedStyle(() => ({
     transform: [
@@ -75,6 +95,21 @@ const PanResponders = () => {
         console.log('====================================');
         console.log(position.value);
       }}></Button>
+
+      <GestureDetector gesture={showCom}>
+        <View style={{ width: '100%', height: 40, backgroundColor: 'rgba(123, 186, 235, 1)' }}>
+          <Animated.View style={{
+            position: 'absolute',
+            transform: [
+              { translateY: -20 },
+              { translateX: dX }
+            ]
+          }}>
+            {show && <Text>active text</Text>}
+          </Animated.View>
+        </View>
+      </GestureDetector>
+
     </GestureHandlerRootView>
   );
 };

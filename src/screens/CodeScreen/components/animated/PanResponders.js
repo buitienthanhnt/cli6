016d92397bdaa7a9a6c72dabf3407f30ab871459
tha nan <buitienthanhnt@gmail.com>
2024-04-26 15:30:22 +0700

    deev-typeScr: Update

--- src/screens/CodeScreen/components/animated/PanResponders.js
+++ src/screens/CodeScreen/components/animated/PanResponders.js
@@ -1,16 +1,31 @@
-import React, { useRef, useState } from 'react';
-import { View, StyleSheet, PanResponder, Text, Button } from 'react-native';
-import Animated, { ReduceMotion, runOnJS, useAnimatedStyle, useSharedValue, withDecay, withRepeat, withSequence, withTiming } from 'react-native-reanimated';
-import { GestureDetector, Gesture } from 'react-native-gesture-handler';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { check, request, openSettings, PERMISSIONS, RESULTS } from 'react-native-permissions';
+import React, {useCallback, useRef, useState} from 'react';
+import {View, StyleSheet, PanResponder, Text, Button} from 'react-native';
+import Animated, {
+  ReduceMotion,
+  runOnJS,
+  useAnimatedStyle,
+  useSharedValue,
+  withDecay,
+  withRepeat,
+  withSequence,
+  withTiming,
+} from 'react-native-reanimated';
+import {GestureDetector, Gesture} from 'react-native-gesture-handler';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import {
+  check,
+  request,
+  openSettings,
+  PERMISSIONS,
+  RESULTS,
+} from 'react-native-permissions';
 
 const END_POSITION = 200;
 
 const PanResponders = () => {
   const [lastX, setLastX] = useState(0);
   const [lastY, setLastY] = useState(0);
-  const [color, setColor] = useState('green')
+  const [color, setColor] = useState('green');
 
   const w = useSharedValue(60);
   const h = useSharedValue(60);
@@ -24,16 +39,16 @@ const PanResponders = () => {
   const [show, setShow] = useState(false);
 
   const panGesture = Gesture.Pan()
-    .onTouchesDown((e) => {
+    .onTouchesDown(e => {
       // doi mau
-      runOnJS(setColor)('violet')
+      runOnJS(setColor)('violet');
 
       // bo vien
-      radius.value = 8
+      radius.value = 8;
 
       // thu nho
-      w.value = withTiming(55, { duration: 200 })
-      h.value = withTiming(55, { duration: 200 })
+      w.value = withTiming(55, {duration: 200});
+      h.value = withTiming(55, {duration: 200});
 
       // xoay
       // rotate.value = withRepeat(
@@ -42,107 +57,147 @@ const PanResponders = () => {
       //   true
       // )
     })
-    .onUpdate((e) => {
+    .onUpdate(e => {
       // console.log(e.translationX);
       tranX.value = lastX + e.translationX;
       tranY.value = lastY + e.translationY;
     })
-    .onEnd((e) => {
-      runOnJS(setLastX)(lastX + e.translationX)
-      runOnJS(setLastY)(lastY + e.translationY)
-    }).onTouchesUp(() => {
-      runOnJS(setColor)('green') // doi lai mau
+    .onEnd(e => {
+      runOnJS(setLastX)(lastX + e.translationX);
+      runOnJS(setLastY)(lastY + e.translationY);
+    })
+    .onTouchesUp(() => {
+      runOnJS(setColor)('green'); // doi lai mau
       radius.value = 0; // het bo vien
       // rotate.value = 0
-      w.value = withTiming(60, { duration: 200 })
-      h.value = withTiming(60, { duration: 200 })
+      w.value = withTiming(60, {duration: 200});
+      h.value = withTiming(60, {duration: 200});
     });
 
-  const showCom = Gesture.Pan().onTouchesDown((e) => {
-    // console.log('====================================');
-    // console.log(e);
-    // console.log('====================================');
-    runOnJS(setOnX)(e.changedTouches[0].x)
-    runOnJS(setShow)(true)
-    dX.value = e.changedTouches[0].x;
-  }).onUpdate((e) => {
-    dX.value = onX + e.translationX;
-  }
-  ).onEnd((e) => {
-    // 
-    runOnJS(setShow)(false);
-    runOnJS(setOnX)(onX + e.translationX)
-  })
+  const onDown = useCallback(
+    data => {
+      setOnX(data);
+      setShow(true);
+      dX.value = data;
+    },
+    [dX],
+  );
+
+  const onEnd = useCallback(
+    data => {
+      setOnX(onX + data);
+      setShow(false);
+    },
+    [onX],
+  );
+
+  const showCom = Gesture.Pan()
+    .onTouchesDown(e => {
+      runOnJS(onDown)(e.changedTouches[0].x);
+    })
+    .onUpdate(e => {
+      dX.value = onX + e.translationX;
+    })
+    .onEnd(e => {
+      runOnJS(onEnd)(e.translationX);
+    })
+    .onTouchesUp(e => {
+      runOnJS(setShow)(false);
+    });
 
   const animatedStyle = useAnimatedStyle(() => ({
     transform: [
-      { translateX: tranX.value },
-      { translateY: tranY.value },
-      { rotateZ: `${rotate.value * 360}deg` }
+      {translateX: tranX.value},
+      {translateY: tranY.value},
+      {rotateZ: `${rotate.value * 360}deg`},
     ],
     width: w.value,
     height: h.value,
     backgroundColor: color,
-    borderRadius: radius.value
+    borderRadius: radius.value,
   }));
 
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
+    <GestureHandlerRootView style={{flex: 1}}>
       <GestureDetector gesture={panGesture}>
         <Animated.View style={animatedStyle} />
       </GestureDetector>
-
-      <Button title='show x' onPress={() => {
-        console.log('====================================');
-        console.log(position.value);
-      }}></Button>
-
       <GestureDetector gesture={showCom}>
-        <View style={{ width: '100%', height: 40, backgroundColor: 'rgba(123, 186, 235, 1)', transform: [{ scaleY: 1.2 }] }}>
-          <Animated.View style={{
-            position: 'absolute',
-            transform: [
-              { translateY: -20 },
-              { translateX: dX },
-            ]
+        <View
+          style={{
+            width: '100%',
+            height: 40,
+            backgroundColor: 'rgba(123, 186, 235, 1)',
+            transform: [{scaleY: 1.2}],
           }}>
+          <Animated.View
+            style={{
+              transform: [{translateX: dX}],
+              height: 40,
+              width: 2,
+              backgroundColor: 'red',
+            }}
+          />
+          <Animated.View
+            style={{
+              position: 'absolute',
+              transform: [{translateY: -20}, {translateX: dX}],
+            }}>
             {show && <Text>active text</Text>}
           </Animated.View>
         </View>
       </GestureDetector>
+      <Text />
 
-      <Button title='request Per' onPress={() => {
-        check(PERMISSIONS.IOS.MEDIA_LIBRARY)
-          .then((result) => {
-            switch (result) {
-              case RESULTS.UNAVAILABLE:
-                console.log('This feature is not available (on this device / in this context)');
-                break;
-              case RESULTS.DENIED:
-                console.log('The permission has not been requested / is denied but requestable');
-                break;
-              case RESULTS.LIMITED:
-                console.log('The permission is limited: some actions are possible');
-                break;
-              case RESULTS.GRANTED:
-                console.log('The permission is granted');
-                break;
-              case RESULTS.BLOCKED:
-                console.log('The permission is denied and not requestable anymore');
-                break;
-            }
-          })
-          .catch((error) => {
-            // …
+      <Button
+        title="request Per"
+        onPress={() => {
+          check(PERMISSIONS.IOS.MEDIA_LIBRARY)
+            .then(result => {
+              switch (result) {
+                case RESULTS.UNAVAILABLE:
+                  console.log(
+                    'This feature is not available (on this device / in this context)',
+                  );
+                  break;
+                case RESULTS.DENIED:
+                  console.log(
+                    'The permission has not been requested / is denied but requestable',
+                  );
+                  break;
+                case RESULTS.LIMITED:
+                  console.log(
+                    'The permission is limited: some actions are possible',
+                  );
+                  break;
+                case RESULTS.GRANTED:
+                  console.log('The permission is granted');
+                  break;
+                case RESULTS.BLOCKED:
+                  console.log(
+                    'The permission is denied and not requestable anymore',
+                  );
+                  break;
+              }
+            })
+            .catch(error => {
+              // …
+            });
+
+          request(PERMISSIONS.IOS.MEDIA_LIBRARY).then(result => {
+            console.log('---->', result);
           });
 
-        request(PERMISSIONS.IOS.MEDIA_LIBRARY).then((result) => {
-          console.log('---->', result);
-        });
-
-        openSettings().catch(() => console.warn('cannot open settings'));
-      }}></Button>
-
+          openSettings().catch(() => console.warn('cannot open settings'));
+        }}
+      />
+      <Text />
+      <Button
+        title={'run'}
+        onPress={() => {
+          dX.value = withRepeat(withTiming(200, {duration: 1000}), 10, true);
+        }}
+      />
     </GestureHandlerRootView>
   );
 };
--- src/screens/CodeScreen/components/animated/index.js
+++ src/screens/CodeScreen/components/animated/index.js
@@ -1,130 +1,144 @@
-import React, { useCallback, useEffect, useState, useRef } from "react";
-import { View, Text, TouchableOpacity, FlatList, Animated, Easing } from 'react-native';
+import React, {useCallback, useEffect, useState, useRef} from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  FlatList,
+  Animated,
+  Easing,
+} from 'react-native';
 
 const screens = [
-    {
-        name: 'Animate1',
-        component: 'Animate1'
-    },
-    {
-        name: 'SwipeListViews',
-        component: 'SwipeListViews'
-    },
-    {
-        name: 'pull handle',
-        component: 'PanResponders'
-    },
-    {
-        name: 'TabViewExample',
-        component: 'TabViewExample'
-    },
-    {
-        name: 'SwipeBtn',
-        component: 'SwipeBtn'
-    },
-    {
-        name: 'Slide Image ScrollViews',
-        component: 'ScrollViews'
-    },
-    {
-        name: 'to FadeInView',
-        component: 'FadeInView'
-    },
-    {
-        name: 'to ExAnimated1',
-        component: 'ExAnimated1'
-    },
-    {
-        name: 'to ExAnimated2',
-        component: 'ExAnimated2'
-    },
-    {
-        name: 'to ExAnimated3',
-        component: 'ExAnimated3'
-    },
-    {
-        name: 'to show message',
-        component: 'ExAnimated4'
-    },
-    {
-        name: 'to show delay',
-        component: 'ExAnimated5'
-    }, // Reanimated1
-    {
-        name: 'to Reanimated1',
-        component: 'Reanimated1'
-    },
-    {
-        name: 'to Reanimated2',
-        component: 'Reanimated2'
-    },
+  {
+    name: 'Animate1',
+    component: 'Animate1',
+  },
+  {
+    name: 'SwipeListViews',
+    component: 'SwipeListViews',
+  },
+  {
+    name: 'pull handle',
+    component: 'PanResponders',
+  },
+  {
+    name: 'TabViewExample',
+    component: 'TabViewExample',
+  },
+  {
+    name: 'SwipeBtn',
+    component: 'SwipeBtn',
+  },
+  {
+    name: 'Slide Image ScrollViews',
+    component: 'ScrollViews',
+  },
+  {
+    name: 'to FadeInView',
+    component: 'FadeInView',
+  },
+  {
+    name: 'to ExAnimated1',
+    component: 'ExAnimated1',
+  },
+  {
+    name: 'to ExAnimated2',
+    component: 'ExAnimated2',
+  },
+  {
+    name: 'to ExAnimated3',
+    component: 'ExAnimated3',
+  },
+  {
+    name: 'to show message',
+    component: 'ExAnimated4',
+  },
+  {
+    name: 'to show delay',
+    component: 'ExAnimated5',
+  }, // Reanimated1
+  {
+    name: 'to Reanimated1',
+    component: 'Reanimated1',
+  },
+  {
+    name: 'to Reanimated2',
+    component: 'Reanimated2',
+  },
 ];
 
-const ExAnimated = ({ navigation }) => {
+const ExAnimated = ({navigation}) => {
+  const renderItem = useCallback(
+    ({item, index}) => {
+      return <Item item={item} index={index} navigation={navigation} />;
+    },
+    [navigation],
+  );
 
-    const renderItem = useCallback(({ item, index }) => {
-        return <Item item={item} index={index} navigation={navigation}></Item>
-    }, []);
-
-    return (
-        <View style={{ flex: 1, padding: 10,}}>
-            <FlatList
-                data={screens}
-                numColumns={2}
-                horizontal={false}
-                renderItem={renderItem}
-            // ItemSeparatorComponent={()=>{
-            //     return <View style={{height: 5}}></View>
-            // }}
-
-            >
-            </FlatList>
-        </View>
-    )
+  return (
+    <View style={{flex: 1, padding: 10}}>
+      <FlatList
+        data={screens}
+        numColumns={2}
+        horizontal={false}
+        renderItem={renderItem}
+        // ItemSeparatorComponent={()=>{
+        //     return <View style={{height: 5}}></View>
+        // }}
+      />
+    </View>
+  );
 };
 
-const Item = ({ navigation, item, index }) => {
+const Item = ({navigation, item, index}) => {
+  const opacity = useRef(new Animated.Value(0)).current;
 
-    const opacity = useRef(new Animated.Value(0)).current;
+  useEffect(() => {
+    Animated.timing(opacity, {
+      toValue: 1,
+      duration: index * 200,
+      easing: Easing.linear,
+      useNativeDriver: true,
+    }).start();
+  }, [index, opacity]);
 
-    useEffect(() => {
-        Animated.timing(opacity, {
-            toValue: 1,
-            duration: index * 200,
-            easing: Easing.linear,
-            useNativeDriver: true,
-        }).start()
-    }, []);
+  const randomColor = useCallback(() => {
+    return `rgba(${randomValue(256)}, ${randomValue(256)}, ${randomValue(
+      256,
+    )}, 1)`;
+  }, [randomValue]);
 
-    const randomColor = useCallback(() => {
-        return `rgba(${randomValue(256)}, ${randomValue(256)}, ${randomValue(256)}, 1)`;
-    }, []);
+  const redirectView = useCallback(
+    viewName => {
+      navigation.navigate(viewName);
+    },
+    [navigation],
+  );
 
-    const redirectView = useCallback((viewName) => {
-        navigation.navigate(viewName, {navigation: navigation});
-    }, [navigation]);
+  const randomValue = useCallback(max => {
+    return Math.floor(Math.random() * max);
+  }, []);
 
-    const randomValue = useCallback((max) => {
-        return Math.floor(Math.random() * max);
-    }, []);
-
-    return (
-        <Animated.View style={{
-            height: 60, flex: 1, backgroundColor: randomColor(),
-            justifyContent: 'center', alignItems: 'center', margin: 2,
-            borderRadius: 10,
-            opacity: opacity
+  return (
+    <Animated.View
+      style={{
+        height: 60,
+        flex: 1,
+        backgroundColor: randomColor(),
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: 2,
+        borderRadius: 10,
+        opacity: opacity,
+      }}>
+      <TouchableOpacity
+        style={{}}
+        onPress={() => {
+          redirectView(item.component);
         }}>
-            <TouchableOpacity
-                style={{
-                }}
-                onPress={() => {
-                    redirectView(item.component);
-                }}
-            >
-                <Text>{item.name}</Text>
-            </TouchableOpacity>
-        </Animated.View>
-    )
-}
+        <Text>{item.name}</Text>
+      </TouchableOpacity>
+    </Animated.View>
+  );
+};
 export default ExAnimated;

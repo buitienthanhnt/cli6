86892fc399db73d94589b8fe1fd56a15c4f12e80
buitienthanhnt@gmail.com <namtanvietnam>
2024-06-13 13:49:42 +0700

    dev: update yarn version moi co the bij nhieu loi voi cac thu vien

--- babel.config.js
+++ babel.config.js
@@ -1,33 +1,32 @@
 module.exports = {
-			presets: ['module:metro-react-native-babel-preset'],
-			plugins: [
-			    'nativewind/babel',
-				'react-native-reanimated/plugin',
-				[
-					require.resolve('babel-plugin-module-resolver'),
-					{
-						root: ['./src'],
-						extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
-						alias: {
-						 'ios': 	  './ios/',
-						 'android':   './android/',
-						 '@assets':   './src/assets',
-						 '@config':   './src/config',
-						 '@hooks': 	  './src/hooks',
-						 '@screens':  './src/screens',
-						 '@utils': 	  './src/utils',
-						 '@bottoms':  './src/bottoms',
-						 '@redux': 	  './src/redux',
-						 '@services': './src/services',
-						 '@locales':  './src/locales',
-						 '@netWork':  './src/netWork',
-						 '@elements': './src/elements',
-						 '@styles':   './src/styles',
-						 '@queries':  './src/queries', // constants
-						 '@constants':  './src/constants',
-						},
-					},
-				],
-
-			]
-		}; 
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    'nativewind/babel',
+    'react-native-reanimated/plugin',
+    [
+      require.resolve('babel-plugin-module-resolver'),
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          ios: './ios/',
+          android: './android/',
+          '@assets': './src/assets',
+          '@config': './src/config',
+          '@hooks': './src/hooks',
+          '@screens': './src/screens',
+          '@utils': './src/utils',
+          '@bottoms': './src/bottoms',
+          '@redux': './src/redux',
+          '@services': './src/services',
+          '@locales': './src/locales',
+          '@netWork': './src/netWork',
+          '@elements': './src/elements',
+          '@styles': './src/styles',
+          '@queries': './src/queries', // constants
+          '@constants': './src/constants',
+        },
+      },
+    ],
+  ],
+};
--- src/screens/CodeScreen/VideoPlay.js
+++ src/screens/CodeScreen/VideoPlay.js
@@ -2,21 +2,40 @@ import React, {useEffect, useState} from 'react';
 import {View, StyleSheet, Button, Dimensions} from 'react-native';
 import Video from 'react-native-video'; // https://blog.logrocket.com/adding-videos-react-native-react-native-video/
 import {videosUrl} from './testVideoUrl';
+import Animated, {
+  runOnJS,
+  useAnimatedReaction,
+  useSharedValue,
+} from 'react-native-reanimated';
 
 // https://www.pexels.com/search/videos/gif/
 const VideoPlay = () => {
+  const VideoPlayer = Animated.createAnimatedComponent(Video);
   const [pause, setPause] = useState(false);
   return (
     <View style={{flex: 1, paddingHorizontal: 5}}>
-      <Video
+      <VideoPlayer
         source={{
-          uri: videosUrl[0],
+          uri: 'https://alphonso-uppy.jmango360.dev/files/vjuz0dfqo7gz0dnzs6xmv7zt/1496953604',
+          type: 'm3u8',
         }}
         paused={pause} // make it start
         style={styles.backgroundVideo} // any style you want
         repeat={true} // make it a loop
         // type={'m3u8'} // định dạng này hỗ trợ tốt hơn
         resizeMode={'cover'}
+        poster={
+          'https://scontent.fhan2-5.fna.fbcdn.net/v/t39.30808-6/448182425_426022043682243_3046730575109270469_n.jpg?stp=dst-jpg_s600x600&_nc_cat=104&ccb=1-7&_nc_sid=5f2048&_nc_ohc=GQokLNZ-2S0Q7kNvgFAFuTA&_nc_ht=scontent.fhan2-5.fna&oh=00_AYBqjIc1qKxxyaZqrEllb6W5a-BCLvE3e8Hd91TolRN5xQ&oe=6670427E'
+        }
+        onError={error => {
+          console.log('----', error);
+        }}
+        pictureInPicture={false}
+        playInBackground={false}
+        playWhenInactive={false}
+        allowsExternalPlayback={false}
+        preventsDisplaySleepDuringVideoPlayback={true}
+        hideShutterView={true}
       />
       <View style={{height: 10}} />
       <Button
@@ -33,6 +52,7 @@ const VideoPlay = () => {
 const styles = StyleSheet.create({
   backgroundVideo: {
     height: ((Dimensions.get('screen').width - 10) / 4) * 3,
+    width: '100%',
     marginTop: 10,
     borderRadius: 4,
   },
